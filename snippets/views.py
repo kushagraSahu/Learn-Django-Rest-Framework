5ca0d9f162d06615d35975912076019f2ecc0d47
Kushagra Sahu <kushagrasahu.ed@gmail.com>
2016-06-14 02:24:55 +0530

    Generic class based views

--- snippets/views.py
+++ snippets/views.py
@@ -1,35 +1,44 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import mixins
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
+#REST framework provides a set of already mixed-in generic views that we can use.
+class SnippetList(generics.ListCreateAPIView):
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
+
+
 # The base class provides the core functionality, and the mixin classes provide the .list() and .create() actions. We're then explicitly binding the get and post methods to the
-# appropriate actions.
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
-	permission_classes = (IsAuthenticated,)
+# # appropriate actions.
+# class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+# 	permission_classes = (IsAuthenticated,)
 
-	queryset = Snippet.objects.all()
-	serializer_class = SnippetSerializer
+# 	queryset = Snippet.objects.all()
+# 	serializer_class = SnippetSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.list(request, *args, **kwargs)
+# 	def get(self, request, *args, **kwargs):
+# 		return self.list(request, *args, **kwargs)
 
-	def post(self, request, *args, **kwargs):
-		return self.create(request, *args, **kwargs)
+# 	def post(self, request, *args, **kwargs):
+# 		return self.create(request, *args, **kwargs)
 
-#using the GenericAPIView class to provide the core functionality, and adding in mixins to provide the .retrieve(), .update() and .destroy() actions.
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
-	permission_classes = (IsAuthenticated,)
+# #using the GenericAPIView class to provide the core functionality, and adding in mixins to provide the .retrieve(), .update() and .destroy() actions.
+# class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+# 	permission_classes = (IsAuthenticated,)
 
-	queryset = Snippet.objects.all()
-	serializer_class = SnippetSerializer
+# 	queryset = Snippet.objects.all()
+# 	serializer_class = SnippetSerializer
 
-	def get(self, request, *args, **kwargs):
-		return self.retrieve(request, *args, **kwargs)
+# 	def get(self, request, *args, **kwargs):
+# 		return self.retrieve(request, *args, **kwargs)
 
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
+# 	def put(self, request, *args, **kwargs):
+# 		return self.update(request, *args, **kwargs)
 	
-	def delete(self, request, *args, **kwargs):
-		return self.destroy(request, *args, **kwargs)		
+# 	def delete(self, request, *args, **kwargs):
+# 		return self.destroy(request, *args, **kwargs)		
